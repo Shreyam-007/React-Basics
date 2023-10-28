a000ad37dcab0eca1432d5397ddc68487dc98056
Shreyam <shreyamamritansh11@gmail.com>
2023-10-29 00:53:32 +0530

    interview related question solved

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,13 +5,27 @@ import "./App.css";
 
 function App() {
 
-  let [counter, setCounter] = useState(15)
+  const [counter, setCounter] = useState(15)
 
   // let counter = 15;
   const addValue = () => {
     // counter++
     if (counter <= 19) {
-      setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // console.log(counter); 
+
+      //useState variables and UI updation ko batches me bhejta h, or agar batches me same UI updation h toh usse ignore kr deta h
+      // (here the function calls will be sent in batches. So, react will see them as the same operation and perform it only once. So, the counter will increase by only 1 count.)
+      // but hum callback fxn ko use krke differentiate kr sakte h.
+      // To increase it by four counts one by one, use the callback which the setCounter accepts and increase it one by one. So, the first function will be called first, the callback will be executed and the next function call will be executed.
+
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
     }
     // console.log(`value: ${counter}`);
   };
